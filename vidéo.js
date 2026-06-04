b010bc3c6b1eae455519ce8c52bc18232f0fa7e1
nbertrandmacadamtonic-create <nbertrandmacadamtonic@gmail.com>
2026-06-04 22:07:41 +0200

    Create vidéo.js

---
+++ vidéo.js
@@ -0,0 +1,10 @@
+const OSTEO_RA_VIDEOS = [
+  {
+    id: "osteo_ra_test_001",
+    title: "Videotest",
+    category: "Test",
+    duration: "",
+    level: "Test",
+    vimeoId: "1198534346"
+  }
+];
